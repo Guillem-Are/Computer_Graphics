d5d63c81399a3697503d052dd93203bd028c7c51
Guillem-Are <guillem.arevalo01@estudiant.upf.edu>
2026-01-21 00:33:16 +0100

    LAB 1 - 2.3 por acavar. particles snowing

---
+++ ParticleSystem.h
@@ -0,0 +1,26 @@
+//
+//  ParticleSystem.h
+//  ComputerGraphics
+//
+//  Created by Guillem Arevalo Morell on 20/1/26.
+//
+class ParticleSystem {
+
+        static const int MAX_PARTICLES = 1000;
+
+        struct Particle {
+                Vector2 position;
+                Vector2 velocity; // Normalized speed and direction of the particle
+                Color color;
+                float acceleration;
+                float ttl; // Time left until the particle expires
+                bool inactive; // Particle is not used/expired, so it can be recreated
+        };
+
+        Particle particles[MAX_PARTICLES];
+
+public:
+        void Init();
+        void Render(Image* framebuffer);
+        void Update(float dt);
+};
