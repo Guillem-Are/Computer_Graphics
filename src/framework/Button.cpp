0ae0b915c22aa0d724e5b7076b1105d11b480f5b
sgonzzal <sara.gonzalez15@estudiant.upf.edu>
2026-01-22 16:38:55 +0100

    ...

---
+++ src/framework/Button.cpp
@@ -0,0 +1,8 @@
+//
+//  Button.cpp
+//  ComputerGraphics
+//
+//  Created by Sara Gonzalez on 22/1/26.
+//
+
+#include "Button.hpp"
---
+++ src/framework/Button.h
@@ -0,0 +1,13 @@
+//
+//  Button.hpp
+//  ComputerGraphics
+//
+//  Created by Sara Gonzalez on 22/1/26.
+//
+
+#ifndef Button_hpp
+#define Button_hpp
+
+#include <stdio.h>
+
+#endif /* Button_hpp */
