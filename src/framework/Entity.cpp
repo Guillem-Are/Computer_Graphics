f8fdc8b788d9a1823c31e795e23bda77ae0009ee
sgonzzal <sara.gonzalez15@estudiant.upf.edu>
2026-01-28 16:19:02 +0100

    Entity class

--- src/framework/Entity.cpp
+++ src/framework/Entity.cpp
@@ -5,4 +5,26 @@
 //  Created by Sara Gonzalez on 28/1/26.
 //
 
-#include "Entity.hpp"
+#include "Entity.h"
+
+void Entity::Render(Image* framebuffer, Camera* camera, const Color& c)
+{
+    // Get the vertices of the mesh and iterate through them
+    std::vector<Vector3> vertices = mesh->GetVertices();
+    
+    for(Vector3 v : vertices){
+        
+        // Transform the vertices from local to world space using the Entity’s model matrix
+        // Project each of the world space vertices to clip space positions using your current camera
+        
+        // Make sure to render only the projected triangles that lay inside the cube
+        // Before drawing each of the triangle lines, convert the clip space positions to screen space using the framebuffer width and height.
+    }
+    
+}
+
+
+void Entity::Update(float seconds_elapsed)
+{
+    
+}
--- src/framework/Entity.h
+++ src/framework/Entity.h
@@ -5,9 +5,27 @@
 //  Created by Sara Gonzalez on 28/1/26.
 //
 
-#ifndef Entity_hpp
-#define Entity_hpp
+#ifndef Entity_h
+#define Entity_h
 
 #include <stdio.h>
+#include "mesh.h"
+#include "framework.h"
+#include "camera.h"
+#include "image.h"
 
-#endif /* Entity_hpp */
+
+class Entity {
+public:
+    // ATTRIBUTES:
+    Mesh* mesh;
+    Matrix44 model;
+    
+    // METHODS
+    
+    void Render(Image* framebuffer, Camera* camera, const Color& c);
+    void Update(float seconds_elapsed);
+};
+
+
+#endif /* Entity_h */
