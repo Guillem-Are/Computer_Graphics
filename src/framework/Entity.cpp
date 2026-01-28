c00e6a9d5dd7db5ed2c32bdfcc1af28ccda607e8
sgonzzal <sara.gonzalez15@estudiant.upf.edu>
2026-01-28 16:16:41 +0100

    entyty class

---
+++ src/framework/Entity.cpp
@@ -0,0 +1,8 @@
+//
+//  Entity.cpp
+//  ComputerGraphics
+//
+//  Created by Sara Gonzalez on 28/1/26.
+//
+
+#include "Entity.hpp"
---
+++ src/framework/Entity.h
@@ -0,0 +1,13 @@
+//
+//  Entity.hpp
+//  ComputerGraphics
+//
+//  Created by Sara Gonzalez on 28/1/26.
+//
+
+#ifndef Entity_hpp
+#define Entity_hpp
+
+#include <stdio.h>
+
+#endif /* Entity_hpp */
